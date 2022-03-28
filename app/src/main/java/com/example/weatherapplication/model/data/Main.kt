d935777159dfbe406eae06515bcc40d5652c10c7
VITYQ <vitaly.sorokin14@gmail.com>
2022-03-28 19:00:35 +0300

    Добавлены стандартные значения

--- app/src/main/java/com/example/weatherapplication/model/data/Main.kt
+++ app/src/main/java/com/example/weatherapplication/model/data/Main.kt
@@ -5,6 +5,6 @@ data class Main(
     val feels_like: Float?,
     val temp_min: Float?,
     val temp_max: Float?,
-    val pressure: Int?,
-    val humidity: Int?
+    val pressure: Int,
+    val humidity: Int
 )
